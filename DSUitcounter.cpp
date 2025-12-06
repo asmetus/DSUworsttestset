9c69356cf9977199596b88b9ef410a298af697ec
asmetus <n89818096333@yandex.ru>
2025-12-06 16:41:01 +0300

    Update DSUitcounter.cpp

--- DSUitcounter.cpp
+++ DSUitcounter.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
-#include <utility>
 #include <vector>
-#include <string>
-#include <cstdio>
-#include <set>
-#include <cstdlib>
-#include <fstream>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <map>
-#include <ctime>
-#include <random>
-#include <unordered_set>
-#include <chrono>
-#include <queue>
-#include <unordered_map>
-#include <cassert>
-#include <bitset>
 
 using namespace std;
 
@@ -61,3 +43,4 @@ signed main(){
     }
     cout << ttime << endl;
 }
+
